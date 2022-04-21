4e3ba1ef5af0f6f9971f5d85ff6d6a912a349157
j-lara-2021 <j.lara.2021@alumnos.urjc.es>
2022-04-21 10:13:25 +0200

    Practice 6

--- P6_pathlib/Seq2-server.py
+++ P6_pathlib/Seq2-server.py
@@ -1,56 +1,115 @@
 import http.server
 import socketserver
-import seq_functions as seq
+import termcolor
 from pathlib import Path
 import jinja2 as j
 from urllib.parse import parse_qs, urlparse
 
 HTML_FOLDER = "./html/"
-LIST_SEQUENCES = ["ATGAGCGAGGTTATA", "GGGATCGAGAGCTAGC", "ATGCTAGGGACCCC", "TAGCTAGTCAGCCCGGGTT", "GGGTTACTTATCAC"]
-LIST_GENES = ["ADA", "FRAT1", "FXN", "RNU6_269P", "U5"]
+LIST_SEQUENCES = ["ACGTCCAGTAAA", "ACGTAGTTTTTAAACCC", "GGGTAAACTACG",
+                  "CGTAGTACGTA", "TGCATGCCGAT", "ATATATATATATATATATA"]
+LIST_GENES = ["ADA", "FRAT1", "FXN", "RNU5A", "U5"]
 
 def read_html_file(filename):
     contents = Path(HTML_FOLDER + filename).read_text()
     contents = j.Template(contents)
     return contents
 
+
+def count_bases(seq):
+    d = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for b in seq:
+        d[b] += 1
+
+    total = sum(d.values())
+    for k,v in d.items():
+        d[k] = [v, (v * 100) / total]
+    return d
+
+
+def convert_message(base_count):
+    message = ""
+    for k,v in base_count.items():
+        message += "<p> " + k + ": " +  str(v[0]) + " (" + str(v[1]) + "%)" + "</p>"
+    return message
+
+def comp_str(sequence):
+    comp = sequence.replace('C', 'g').replace("G", "c").replace("A", "t").replace("T", "a").upper()
+    return comp
+
+def info_operation(arg):
+    base_count = count_bases(arg)
+    response = "<p> Sequence: " + arg + "</p>"
+    response += "<p> Total length: " + str(len(arg)) + "</p>"
+    response += convert_message(base_count)
+    return response
+
+# Define the Server's port
 PORT = 4876
+
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
 
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happytest server: It always returns a message saying
+        # that everything is ok
         url_path = urlparse(self.path)
         path = url_path.path
         arguments = parse_qs(url_path.query)
-        route = self.path
-        if path == "/":
+        print("The old path was", self.path)
+        print("The new path is", url_path.path)
+        print("arguments", arguments)
+        # Message to send back to the clinet
+        if self.path == "/":
             contents = read_html_file("index.html")\
-                .render(context={"n_sequences": len(LIST_SEQUENCES), "genes": LIST_GENES})
+                .render(context=
+                        {"n_sequences": len(LIST_SEQUENCES),
+                         "genes": LIST_GENES})
         elif path == "/ping":
             contents = read_html_file(path[1:] + ".html").render()
         elif path == "/get":
             n_sequence = int(arguments["n_sequence"][0])
             sequence = LIST_SEQUENCES[n_sequence]
             contents = read_html_file(path[1:] + ".html")\
-                .render(context={"n_sequence": n_sequence, "sequence": sequence})
+                .render(context = {
+                "n_sequence": n_sequence,
+                "sequence": sequence
+            })
         elif path == "/gene":
             gene_name = arguments["gene_name"][0]
-            sequence = Path("./genes/" + gene_name + ".txt").read_text()
+            sequence = Path("./sequences/" + gene_name + ".txt").read_text()
             contents = read_html_file(path[1:] + ".html") \
-                .render(context={"gene_name": gene_name, "sequence": sequence})
+                .render(context={
+                "gene_name": gene_name,
+                "sequence": sequence
+            })
         elif path == "/operation":
             sequence = arguments["sequence"][0]
             operation = arguments["operation"][0]
-            if operation == "rev":
-                contents = read_html_file(path[1:] + ".html") .render(context={"operation": operation, "sequence": sequence[::-1])
-            elif
+            contents = read_html_file(path[1:] + ".html") \
+                .render(context={
+                "sequence": sequence,
+                "operation": operation,
+                "rev": sequence[::-1],
+                "info": info_operation(sequence),
+                "comp": comp_str(sequence)
+            })
+        else:
+            contents = open("html/error.html", "r").read()
 
-                """when you pass info opertaion string to html write <br> instead of \n or embed them in a <p>"""
-
-
-
+        # Generating the response message
         self.send_response(200)  # -- Status line: OK!
 
         # Define the content-type header:
@@ -65,6 +124,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         return
 
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
 Handler = TestHandler
 
 # -- Open the socket server
